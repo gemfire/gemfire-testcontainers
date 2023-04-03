3fc4aa4c110ccfde6974bb9a44bdb7b117d52a96
Jens Deppe <jdeppe@vmware.com>
2023-04-03 12:47:24 -0700

    Add some javadocs

--- src/main/java/com/vmware/gemfire/GemFireClusterContainer.java
+++ src/main/java/com/vmware/gemfire/GemFireClusterContainer.java
@@ -13,6 +13,33 @@ import org.testcontainers.images.builder.Transferable;
 import org.testcontainers.utility.Base58;
 import org.testcontainers.utility.DockerImageName;
 
+/**
+ * Class used to instantiate a GemFire cluster. The default settings will launch a locator and 2
+ * servers.
+ * <p>
+ * Example:
+ * <pre>
+ *   try (GemFireClusterContainer<?> cluster = new GemFireClusterContainer<>()) {
+ *     cluster.start();
+ *
+ *     cluster.gfsh(true, "list members",
+ *         "create region --name=FOO --type=REPLICATE",
+ *         "describe region --name=FOO");
+ *
+ *     ClientCache cache = new ClientCacheFactory()
+ *         .addPoolLocator("localhost", cluster.getLocatorPort())
+ *         .create();
+ *
+ *     Region<Integer, String> region =
+ *         cache.<Integer, String>createClientRegionFactory(ClientRegionShortcut.PROXY)
+ *             .create("FOO");
+ *
+ *     region.put(1, "Hello World");
+ *
+ *     assertThat(region.get(1)).isEqualTo("Hello World");
+ *   }
+ * </pre>
+ */
 public class GemFireClusterContainer<SELF extends GemFireClusterContainer<SELF>> extends GenericContainer<SELF> {
 
   private static final Logger LOG = LoggerFactory.getLogger(GemFireClusterContainer.class);
@@ -95,18 +122,38 @@ public class GemFireClusterContainer<SELF extends GemFireClusterContainer<SELF>>
     locator.stop();
   }
 
+  /**
+   * The given local path will be made available on the classpath of the container.
+   *
+   * @param classpath which is resolved to an absolute path and then bind-mounted to each container
+   *                  at the location {@code /build}. This path is added to the classpath of the
+   *                  started JVM instance.
+   */
   public SELF withClasspath(String classpath) {
     memberConfigs.get(CLUSTER_INDEX).addConfig(container -> container
         .withFileSystemBind(classpath, "/build", BindMode.READ_ONLY));
     return self();
   }
 
+  /**
+   * Add the given gemfire property to a specific started GemFire server instance.
+   * @param serverIndex the instance to target. Instances are numbered from 1.
+   * @param name the name of the property. The property will automatically be prefixed with
+   *        {@code gemfire.}
+   * @param value the value of the property to set
+   */
   public SELF withGemFireProperty(int serverIndex, String name, String value) {
     memberConfigs.get(serverIndex)
         .addConfig(container -> container.addJvmArg(String.format("-Dgemfire.%s=%s", name, value)));
     return self();
   }
 
+  /**
+   * Open a debug port for a given instance. The provided port will be exposed externally and the
+   * server instance will wait until a debugger connects before continuing.
+   * @param serverIndex the instance to target. Instances are numbered from 1.
+   * @param port the port to use for debugging.
+   */
   public SELF withDebugPort(int serverIndex, int port) {
     memberConfigs.get(serverIndex).addConfig(container -> {
       container.setPortBindings(Collections.singletonList(String.format("%d:%d", port, port)));
@@ -126,10 +173,28 @@ public class GemFireClusterContainer<SELF extends GemFireClusterContainer<SELF>>
     return self();
   }
 
+  /**
+   * Return the port at which the locator is listening. This would be used to configure a
+   * GemFire client to connect.
+   */
   public int getLocatorPort() {
     return locator.getLocatorPort();
   }
 
+  /**
+   * Execute the provided commands as a {@code gfsh} script.
+   * <p>
+   * For example:
+   * <pre>
+   *   cluster.gfsh(true, "list members",
+   *       "create region --name=FOO --type=REPLICATE",
+   *       "describe region --name=FOO");
+   * </pre>
+   * @param logOutput boolean indicating whether to log output. If the script returns a non-zero
+   *                  error code, the output will always be logged.
+   * @param commands a list of commands to execute. There is no need to provide an explicit
+   *                 {@code connect} command unless additional parameters are required.
+   */
   public void gfsh(boolean logOutput, String... commands) {
     if (commands.length == 0) {
       return;
