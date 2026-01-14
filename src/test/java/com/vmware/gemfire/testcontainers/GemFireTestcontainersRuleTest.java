cb9565a5ad699efac7f248f05e4bcea234af73e1
Dmitry Bedrin <Dmitry.Bedrin@gmail.com>
2026-01-14 21:59:13 +0100

    Upgrade to testcontainers 2.0 (#7)

--- src/test/java/com/vmware/gemfire/testcontainers/GemFireTestcontainersRuleTest.java
+++ src/test/java/com/vmware/gemfire/testcontainers/GemFireTestcontainersRuleTest.java
@@ -4,22 +4,28 @@
 
 package com.vmware.gemfire.testcontainers;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Rule;
-import org.junit.Test;
-
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.ClientCacheFactory;
 import org.apache.geode.cache.client.ClientRegionShortcut;
 
+import org.junit.jupiter.api.*;
+
 public class GemFireTestcontainersRuleTest {
 
-  @Rule
   public GemFireCluster cluster = new GemFireCluster()
       .acceptLicense()
       .withGfsh(true, "list members", "create region --name=BAZ --type=REPLICATE");
+
+  @BeforeEach
+  public void setup() {
+    cluster.start();
+  }
+
+  @AfterEach
+  public void teardown() {
+    cluster.close();
+  }
 
   @Test
   public void testSetupWithRule() {
